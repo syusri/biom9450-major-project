d63cd6f8c7b1a01d05dda7bcc456ef37e6921e3b
Shabrina Yusri <s.yusri@student.unsw.edu.au>
2021-11-10 16:03:48 +1100

    dashboard html done

--- dashboard.php
+++ dashboard.php
@@ -12,9 +12,48 @@
 
 <body>
 	<?php
-		include 'index.html';
+		include_once 'index.html';
 	?>
-	
+		<div class="main">
+			<div class="form__container">
+				<div class="form__content">
+					<p class="form__subheading">Choose which medications and diet regimes to display.</p>
+					<!-- Actual form -->
+					<form id="form--dashboard" class="" action="">
+						<!-- Date -->
+						<div class="input__container">
+							<label for="date" type="date">Date</label><br>
+							<input type="date" value="2021-11-22" min="2021-11-22" max="2021-12-05">
+						</div>
+						<!-- Dropdown List of Time of Day -->
+						<div class="input__container">
+							<label for="time">Time of Day</label><br>
+							<select name="dropdown--time" id="dropdown--time">
+								<option value="Morning">Morning</option>
+								<option value="Afternoon">Afternoon</option>
+								<option value="Evening">Evening</option>
+							</select>
+						</div>
+						<!-- Dropdown List of Patients -->
+						<div class="input__container">
+							<label for="patient">Patient</label><br>
+							<select name="dropdown--patients" id="dropdown--patients">
+								<option value="Margaret">Margaret</option>
+								<option value="Bob">Bob</option>
+							</select>
+						</div>
+						<div class="input__container">
+							<input type="submit" value="Go" class="form__submit">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<script type="text/javascript">
+			document.getElementById("dashboard").classList.add("sidenav__link--anchor-primary");
+			document.getElementById("heading").innerText = "Dashboard";
+			document.getElementById("practitioner").innerText = "Dr. Rosalind Franklin";
+		</script>
 </body>
 
 </html>
