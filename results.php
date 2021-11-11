ab32c17085d7cecd601389449829aa6753dab3a6
Shabrina Yusri <s.yusri@student.unsw.edu.au>
2021-11-11 17:44:32 +1100

    added template for everything except for the small form on top

--- results.php
+++ results.php
@@ -19,22 +19,99 @@
 			<div class="breadcrumb">
 					<p><span class="page--previous">Dashboard</span> > <span class="page--current">Medications and Diet Regimes</span></p>
 			</div>
-			<div class="patient__container">
-				<div class="patient__container--highlight">
-					<div class="patient__content">
-						<figure class="patient__picture--mask">
-							<img src="./img/margaret.jpg" class="patient__picture" alt="Picture of Margaret">
-						</figure>
-						<div class="patient__bio--brief">
-							<h2 id="patient-name">Margaret Smith</h2>
-							<p>Room number</p>
+			<!-- Patient Summary -->
+			<section id="patient">
+				<h2>Patient Summary</h2>
+				<div class="section__container">
+					<div class="patient__container--highlight">
+						<div class="patient__highlight">
+							<figure class="patient__highlight--box patient__picture--mask">
+								<img src="./img/margaret.jpg" class="patient__picture" alt="Picture of Margaret">
+							</figure>
+							<h2 class="patient__highlight--box patient__name">
+								Margaret Smith
+							</h2>
+							<p class="patient__highlight--box patient__room--label">
+								Room number
+							</p>
+							<p class="patient__highlight--box patient__room--number">
+								123
+							</p>
+							<p class="patient__highlight--box patient__prac--label">
+								Practitioner
+							</p>
+							<p class="patient__highlight--box patient__prac--name">
+								Dr. Juliette Smith
+							</p>
 						</div>
 					</div>
-
+					<div class="patient__details">
+						<h2 class="patient__details--box patient__profile">Profile</h2>
+						<p class="patient__details--box patient__age--label">Age</p>
+						<p class="patient__details--box patient__age--number">70</p>
+						<p class="patient__details--box patient__gender--label">Gender</p>
+						<p class="patient__details--box patient__gender--gender">Female</p>
+						<p class="patient__details--box patient__dob--label">Date of Birth</p>
+						<p class="patient__details--box patient__dob--date">11/11/1951</p>
+						<p class="patient__details--box patient__weight--label">Weight</p>
+						<p class="patient__details--box patient__weight--number">60kg</p>
+					</div>
 				</div>
-			</div>
-				
+			</section>
+			<!-- Medications -->
+			<section id="medications">
+				<div class="section__heading">
+					<h2 class="subheading">Medications</h2>
+					<p class="section__heading--edit">Edit</p>
+				</div>
+				<div class="section__container">
+					<div class="section__table">
+						<div class="section__table--heading-1">
+							<p class="section__table--heading">Medication Name</p>
+						</div>
+						<div class="section__table--heading-2">
+							<p class="section__table--heading">Dosage</p>	
+						</div>
+						<div class="section__table--heading-3">
+							<p class="section__table--heading">Route</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Diet Regime -->
+			<section id="diet-regime">
+				<div class="section__heading">
+					<h2 class="subheading">Diet Regime</h2>
+					<p class="section__heading--edit">Edit</p>
+				</div>
+				<div class="section__container">
+					<div class="section__table">
+						<div class="section__table--heading-1">
+							<p class="section__table--heading">Diet Regime</p>
+						</div>
+						<div class="section__table--heading-2">
+							<p class="section__table--heading">Description</p>	
+						</div>
+						<div class="section__table--heading-3">
+							<p class="section__table--heading">Exercise Recommendations</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Meal Plan -->
+			<section id="meal-plan">
+				<div class="section__heading">
+					<h2 class="subheading">Meal</h2>
+					<p class="section__heading--edit">Edit</p>
+				</div>
+				<div class="section__container">
+					<div class="section__table">
+					Eggs and toast
+					</div>
+				</div>
+			</section>
 		</div>
+		<!-- JavaScript to change PHP template -->
 		<script type="text/javascript">
 			document.getElementById("dashboard").classList.add("sidenav__link--anchor-primary");
 			document.getElementById("heading").innerText = "Dashboard";
